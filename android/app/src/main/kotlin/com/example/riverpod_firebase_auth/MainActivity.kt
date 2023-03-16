a40c1a7a71bc059dfa4ae1822c0fc3b6b8feedc8
ryotaewamoto <lengtaiyanben@gmail.com>
2023-03-16 20:50:00 +0900

    delete legacy files

--- android/app/src/main/kotlin/com/example/riverpod_firebase_auth/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.riverpod_firebase_auth
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/providers/user_state_provider.dart
+++
@@ -1,6 +0,0 @@
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:riverpod_firebase_auth/repositories/auth_repository.dart';
-
-final userStateProvider = StreamProvider((ref) {
-  return ref.watch(authRepositoryProvider).authStateChanges;
-});
